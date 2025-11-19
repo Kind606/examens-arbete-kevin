c0f78cefc537752ff2a1838b03970377836dc215
Kind606 <kevin.hellgren@hotmail.com>
2025-11-19 14:00:57 +0100

    init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import styles from './page.module.css';
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+
+        <div className={styles.content}>
+          <h1 className={styles.titles}>To get started, edit the page.tsx file.</h1>
+
+          <p className={styles.description}>
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className={styles.link}
+            >
+              Templates
+            </a>
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className={styles.link}
+            >
+              Learning
+            </a>
+          </p>
+        </div>
+
+        <div className={styles.Button}>
+          <a
+            className="button primary"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="logo"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+
+          <a
+            className="button secondary"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
