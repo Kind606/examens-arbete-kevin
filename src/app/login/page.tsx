c55e2ef3ef038b671960413ef387789b3b960971
Kind606 <kevin.hellgren@hotmail.com>
2025-11-24 22:14:23 +0100

    made it so when logged in you cant accses loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,18 @@
+"use client"
 import LoginForm from "@/src/components/loginComps/loginForm";
+import { useAuthStore } from "@/src/store/authStore";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import styles from "./login.module.css";
 
 export default function LoginPage() {
-  
+  const user = useAuthStore((state) => state.user);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/"); 
+  }, [user, router]);
   return (
     <div className={styles.LoginContainer}>
       <div className={styles.Splash}>
