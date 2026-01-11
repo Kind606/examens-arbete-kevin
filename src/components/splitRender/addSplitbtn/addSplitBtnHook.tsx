8e84c6038f7f4a8395c12e841dfc5f23bc7d9b70
Kind606 <kevin.hellgren@hotmail.com>
2026-01-11 21:35:07 +0100

    Made so the user is navigated to the split when added

--- src/components/splitRender/addSplitbtn/addSplitBtnHook.tsx
+++ src/components/splitRender/addSplitbtn/addSplitBtnHook.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import type { Split } from "@/generated/prisma/client";
 import { useAuthStore } from "@/src/store/authStore";
 import { useSplitStore } from "@/src/store/splitStore";
-import type { Split } from "@/generated/prisma/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { addSplitAction } from "./addSplitBtnAction";
 
 export function useAddSplit() {
   const { splits, setSplits } = useSplitStore();
   const user = useAuthStore((state) => state.user);
+  const router = useRouter();
 
   const [showPopover, setShowPopover] = useState(false);
   const [newTitle, setNewTitle] = useState("");
@@ -25,12 +27,12 @@ export function useAddSplit() {
 
     try {
       const newSplit: Split = await addSplitAction(newTitle.trim(), user.id);
-      setSplits([...splits, newSplit]);
+      setNewTitle("");
+      setShowPopover(false);
+      router.push(`/splits/${newSplit.slug}`);
     } catch (err) {
       console.error("Failed to add split:", err);
     }
-
-    setNewTitle("");
     setShowPopover(false);
   };
 
